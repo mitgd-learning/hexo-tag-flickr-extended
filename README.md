5b0bc4b753b2a26108b643dcbc8d4a0965ca7ec8
oakpot <35740886+oakpot@users.noreply.github.com>
2018-01-27 03:39:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# hexo-tag-flickr-extended
-A hexo plugin for inserting image from Flickr in Hexo posts
+## Intro
+hexo-tag-flickr-extended is a Hexo plugin for inserting image from Flickr in Hexo posts.This plugin is based on [plugin](https://github.com/visioncan/hexo-tag-flickr).And add http proxy support.
+
+
+## Installation
+Run command in the hexo root directory :
+
+```npm install hexo-tag-flickr-extended --save```
+
+
+## Usage
+In _config.yml of hexo :
+
+```yaml
+# Flickr API key
+flickr_ext_api_key: your_flickr_api_key
+
+# Http proxy host & port,e.g. http://127.0.0.1:1080
+flickr_ext_proxy_config: your_http_server
+
+# Cache file path & expiry time in milliseconds
+flickr_ext_cache_file_path: flickr_img_cache.json
+flickr_ext_cache_expires: 2592000000
+```
+
+Then,you can insert hexo tag in markdown file like this :
+
+```markdown
+{% flickrExt flickr_image_id size_suffix %} 
+```
+
+Available size suffixes :
+
+
+* **`s`**   small square 75x75
+* **`q`**	large square 150x150
+* **`t`**	thumbnail, 100 on longest side
+* **`m`**	small, 240 on longest side
+* **`n`**	small, 320 on longest side
+* **`-`**	medium, 500 on longest side
+* **`z`**	medium 640, 640 on longest side
+* **`c`**	medium 800, 800 on longest side†
+* **`b`**	large, 1024 on longest side*
+* **`h`**	large 1600, 1600 on longest side†
+* **`k`**	large 2048, 2048 on longest side†
+* **`o`**	original image, either a jpg, gif or png, depending on source format
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,365 @@
+'use strict';
+
+var https = require('https');
+// add https poxy
+var HttpsProxyAgent = require('https-proxy-agent');
+var proxy = hexo.config.flickr_ext_proxy_config || false;
+var agent = proxy ? new HttpsProxyAgent(proxy) : false;
+//
+var Promise = require('bluebird');
+var hexoUtil = require('hexo-util');
+var tagUtil = require('./flickrTagUtil');
+var APIKey = hexo.config.flickr_ext_api_key || false;
+
+
+
+
+
+
+
+//use hexo-fs
+var fs = require('hexo-fs');
+var cacheJson     = [];
+
+// option
+var cacheFilePath = hexo.config.flickr_ext_cache_file_path || false;
+var cachePeriod   = hexo.config.flickr_ext_cache_expires || false;
+var enabledCache  = (!cacheFilePath && !cachePeriod) ? false : true;
+if(!cachePeriod)cachePeriod = Number(cachePeriod);
+
+// load cache file
+if(enabledCache && fs.existsSync(cacheFilePath)){
+  cacheJson = fs.readFileSync(cacheFilePath);
+  if (cacheJson !=="") {
+    cacheJson = JSON.parse(cacheJson);
+  }
+  else {
+    cacheJson=[];
+  }
+  
+}
+
+
+// return used element
+function filterElement(flJson){
+  var returnJson = {"photo":{}};
+  
+  returnJson.photo.title       = flJson.photo.title;
+  returnJson.photo.description = flJson.photo.description;
+  returnJson.photo.farm        = flJson.photo.farm;
+  returnJson.photo.id          = flJson.photo.id;
+  returnJson.photo.media       = flJson.photo.media;
+  returnJson.photo.secret      = flJson.photo.secret;
+  returnJson.photo.server      = flJson.photo.server;
+  returnJson.photo.urls        = flJson.photo.urls;
+  // test add
+  returnJson.photo.originalsecret        = flJson.photo.originalsecret || null;
+  returnJson.photo.originalformat        = flJson.photo.originalformat || null;
+  
+  return returnJson;
+}
+
+
+//push flickr photos
+function pushFlickrJson(flickrJson){
+  if(!enabledCache)return;
+  var isMatch     = false;
+  var filterJson  = filterElement(flickrJson);
+  
+  for(var i = 0; i < cacheJson.length; i++){
+    if(cacheJson[i].fl.photo.id == flickrJson.photo.id){
+      cacheJson[i].fl      = filterJson;
+      isMatch              = true;
+    }
+  }
+  if(!isMatch) cacheJson.push({"fl":filterJson, "expires":0 });
+}
+
+//get cache flickr photos json
+function getFlickrCacheJson(photoId){
+  if(!enabledCache)return null;
+  var d = new Date();
+  for(var i = 0; i < cacheJson.length; i++){
+    if(cacheJson[i].fl.photo.id == photoId){
+      if( cacheJson[i].expires > d.getTime() ){
+        return cacheJson[i].fl;
+      }
+      break;
+    }
+  }
+  return null;
+}
+
+
+// get image size
+function getImageSize( flickrJson, photo_id, photo_size ){
+  var sizeInfo = {"width":0, "height":0};
+  
+  var sizeTable = {
+      "s": "Square",
+      "q": "Large Square",
+      "t": "Thumbnail",
+      "m": "Small",
+      "n": "Small 320",
+      "-" : "Medium",
+      "z": "Medium 640",
+      "c": "Medium 800",
+      "b": "Large",
+      "o": "Original"
+  }
+  
+  if(flickrJson && flickrJson.sizes.size){
+    for(var i=0; i<flickrJson.sizes.size.length; i++){
+      if(flickrJson.sizes.size[i].label == sizeTable[photo_size]){
+        sizeInfo.width  = flickrJson.sizes.size[i].width;
+        sizeInfo.height = flickrJson.sizes.size[i].height;
+      }
+    }
+  }
+  
+  return sizeInfo;
+}
+
+
+//push flickr photos
+function pushImageSizeAndExpress_flickrJson(imageSize, photo_id){
+  if(!enabledCache)return null;
+  var d           = new Date();
+  var expiresTime = d.getTime() + cachePeriod;
+  
+  for(var i=0; i < cacheJson.length; i++){
+    if(cacheJson[i].fl.photo.id == photo_id){
+      cacheJson[i].fl.photo.imgSize = imageSize;
+      cacheJson[i].expires = expiresTime;
+    }
+  }
+}
+
+//
+function addTagHeight( imgAttr , imgSize){
+  var returnImgAttr = imgAttr;
+  if (returnImgAttr) {
+    returnImgAttr["width"] = imgSize.width;
+    returnImgAttr["data-height"] = imgSize.height;
+    return returnImgAttr;
+  }
+}
+
+
+
+/**
+ * promise Flickr API request
+ * @param  {Array} tagArgs Tag args ex: ['15905712665', 'z']
+ * @resolve {Object} image attrs
+ */
+var promiseRequest = function (tagArgs) {
+  if (!APIKey) {
+    throw new Error('flickr_api_key configuration is required');
+  }
+
+  var tag = tagUtil.convertAttr(tagArgs);
+
+  
+    return new Promise(function (resolve, reject) {
+      
+      var flJson = getFlickrCacheJson(tag.id);
+  
+      if(!flJson){
+        // console.log("[api access getInfo]photoId= " + tag.id);
+
+        // test
+
+        var option_obj={
+          host: 'api.flickr.com',
+	        port: '443',
+	        path: '/services/rest/?method=flickr.photos.getInfo' +
+          '&api_key=' + APIKey +
+          '&photo_id=' + tag.id +
+          '&format=json' +
+          '&nojsoncallback=1',
+          method: 'GET',
+          agent: agent,
+        }
+
+        
+
+        https.get(option_obj, function (res) {
+          var data = '';
+
+          res.on('data', function (chunk) {
+            data += chunk;
+          });
+
+          res.on('end', function () {
+            var json = JSON.parse(data);
+            if (json.stat === 'ok') {
+                pushFlickrJson(json);
+                resolve(tagUtil.imgFormat(tag, json));
+                
+            } else {
+              return reject('Flickr Tag Error: ' + tag.id + ' ' +  json.message);
+            }
+          });
+
+        }).on('error', function (e) {
+          return reject('Fetch Flickr API error: ' + e);
+        });
+        
+      }else{
+        // console.log("[cache getInfo]photoId= " + tag.id);
+        return resolve(tagUtil.imgFormat(tag, flJson));
+      }
+      
+    });
+  
+};
+
+
+/**
+ * promise Flickr API request
+ * @param  {Array} tagArgs Tag args ex: ['15905712665', 'z']
+ * @resolve {Object} image attrs
+ */
+var promiseRequest_imageSize = function (tagArgs, returnImgAttr) {
+  if (!APIKey) {
+    throw new Error('flickr_api_key configuration is required');
+  }
+
+  var tag = tagUtil.convertAttr(tagArgs);
+
+  
+    return new Promise(function (resolve, reject) {
+      
+      
+       
+      var flJson = getFlickrCacheJson(tag.id);
+      
+      
+      if(!flJson || !flJson.photo.imgSize){
+        // console.log("[api access getSize]photoId= " + tag.id);
+
+        var option_obj={
+          host: 'api.flickr.com',
+	        port: '443',
+	        path: '/services/rest/?method=flickr.photos.getSizes' +
+          '&api_key=' + APIKey +
+          '&photo_id=' + tag.id +
+          '&format=json' +
+          '&nojsoncallback=1',
+          method: 'GET',
+          agent: agent,
+        }
+
+
+
+
+        https.get(option_obj, function (res) {
+          var data = '';
+
+          res.on('data', function (chunk) {
+            data += chunk;
+          });
+
+          res.on('end', function () {
+            var json = JSON.parse(data);
+           
+            if (json.stat === 'ok') {
+                
+                var imgSize = getImageSize( json, tag.id, tag.size );
+                pushImageSizeAndExpress_flickrJson(imgSize, tag.id);
+                
+                resolve( addTagHeight( returnImgAttr , imgSize) );
+                
+            } else {
+              return reject('Flickr Tag Error: ' + tag.id + ' ' +  json.message);
+            }
+          });
+
+        }).on('error', function (e) {
+          return reject('Fetch Flickr API error: ' + e);
+        });
+        
+      }else{
+        // console.log("[cache getSize]photoId= " + tag.id);
+        // cache file?
+        console.log('from cache');
+        
+        return resolve( addTagHeight(  tagUtil.imgFormat(tag, flJson) , flJson.photo.imgSize ));
+      }
+      
+    });
+  
+};
+
+
+
+
+
+/**
+ * Filckr tag
+ *
+ * Syntax:
+ * ```
+ * {% flickr [class1,class2,classN] photo_id [size] %}
+ * ```
+ */
+hexo.extend.tag.register('flickrExt', function (args, content) {
+  return promiseRequest(args).then(function (imgAttr) {
+    // return hexoUtil.htmlTag('img', imgAttr);
+    return promiseRequest_imageSize(args, imgAttr).then(function (imgAttr_internal) {
+      
+      // console.log(hexoUtil.htmlTag('img', imgAttr_internal));
+      
+      return hexoUtil.htmlTag('img', imgAttr_internal);
+    }, function (err) {
+      hexo.log.error(err);
+    });
+  }, function (err) {
+    hexo.log.error(err);
+  });
+
+}, {async: true});
+
+// write cache file
+hexo.extend.filter.register('after_generate', function(){
+  if(enabledCache){
+    fs.writeFileSync( cacheFilePath , JSON.stringify( cacheJson ) );
+  }
+});
+
+
+/**
+ * For gallery post
+ *
+ * Syntax:
+ * ```
+ * photos:
+ * - flickr photo_id [size]
+ * - flickr photo_id [size]
+ * ```
+ */
+hexo.extend.filter.register('pre', function(data) {
+  if (!data.photos) return data;
+
+  return Promise.map(data.photos, function(photo) {
+    var photoTag = photo.split(' ');
+    if (photoTag[0] !== 'flickr') {
+      return photo;
+    }
+
+    var tagArgs = photoTag.slice(1);
+
+    return promiseRequest(tagArgs).then(function (imgAttr) {
+      return imgAttr.src;
+    }, function (err) {
+      hexo.log.error(err);
+    });
+  }).then(function (results) {
+    data.photos = results;
+    return data;
+  });
+
+});
+
+
+
